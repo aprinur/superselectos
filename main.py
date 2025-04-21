1a5ace87858b81875f7053cae26f031fbcd9cf36
Apri nur <aprinur7@gmail.com>
2025-04-21 09:10:43 +0700

    add more variable in product_parser

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import time
 from requests_html import HTMLSession
 
@@ -39,10 +40,11 @@ def get_product_url(category):
 
     response = session.get(category)
 
-    product_url = response.html.find('.productos-page-inner')
+    product_url = list([i.absolute_links for i in response.html.find('.right-content')][0])
+    product_url = [product_url.remove(i) for i in product_url if 'Id' not in i]
+    
     print(product_url)
-
-
+    
 
 
 def get_html_product(url):
@@ -50,27 +52,36 @@ def get_html_product(url):
 
     response = session.get(url)
     html = response.html
-    # print(html)
+    print(html)
 
     return html
 
 
-def product_parser(html_page, ):
+def product_parser(html_page):    
     name = html_page.find('h1[class="det-nom"]', first=True).text
-    price = html_page.find('div[class="info-middle mb-2"] div[class="right"]', first=True).text
+    current_price = html_page.find('div[class="info-middle mb-2"] div[class="right"]', first=True).text
+    actual_price = html_page.find('div[class="col-md-6 det-info"] span[class="antes"]')
     description = html_page.find('.det-des p', first=True).text
     brand = html_page.find('div[class="info-middle mb-2"] .active', first=True).text if html_page.find('div[class="info-middle mb-2"] .active', first=True) else None
     product_url = html_page.url
 
-
-    return
+    return{
+        "Name": name,
+        "Current Price": current_price,
+        "Actual Price": actual_price,
+        'Description': description,
+        'Brand': brand,
+        'Product_URL': product_url,
+        # 'Category_URL': category_url
+    }
 
 
 def main(url):
     # product_page = get_html_product(url)
-    # product_parser(product_page)
+    # print(product_parser(product_page))
     # get_category_link(url)
     get_product_url(url)
+    ...
 
 if __name__ == "__main__":
     main("https://www.superselectos.com/products?category=0114")
